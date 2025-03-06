268b03bbd1595829edd00a38b8c85e22ebc13da3
K Mills <mustkre8@gmail.com>
2025-03-06 15:29:58 -0800

    Update Error page to use _ for translations and path utility

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,7 @@
 <script>
   import { page } from '$app/stores';
+  import { _ } from '$lib/i18n';
+  import { path } from '$lib/utils/path';
 </script>
 
 <div class="flex flex-col items-center justify-center min-h-[60vh] px-4 py-16">
@@ -12,9 +14,9 @@
   </p>
   
   <a 
-    href="/" 
+    href={path('/')} 
     class="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-8 rounded-lg transition duration-300"
   >
-    Return to Home
+    {$_('nav.home')}
   </a>
 </div>
